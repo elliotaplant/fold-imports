a593d81b741519e5fede1605078b6cc76f32bcc9
Elliot Plant <elliotaplant@gmail.com>
2018-04-17 17:09:28 -0700

    cp

---
+++ spec/fold-imports-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import FoldImportsView from '../lib/fold-imports-view';
+
+describe('FoldImportsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
